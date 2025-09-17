a363758ad0b11490b192f269b9ebf2098333ef2b
Nastassia Yunchyts <nastassia.yunchyts@intexsoft.by>
2025-09-17 13:20:34 +0300

    fix(organizations): add entity graph to OrganizationRepository
    - Change OrganizationRepository

--- src/main/java/com/kydas/build/organizations/OrganizationRepository.java
+++ src/main/java/com/kydas/build/organizations/OrganizationRepository.java
@@ -2,12 +2,22 @@ package com.kydas.build.organizations;
 
 import com.kydas.build.core.crud.BaseRepository;
 import com.kydas.build.core.exceptions.classes.NotFoundException;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrganizationRepository extends BaseRepository<Organization> {
+
+    @EntityGraph(attributePaths = "employees")
+    Optional<Organization> findById(UUID id);
+
+    @EntityGraph(attributePaths = "employees")
+    List<Organization> findAll();
+
     default Organization findByIdOrElseThrow(UUID id) throws NotFoundException {
         return findById(id).orElseThrow(NotFoundException::new);
     }
