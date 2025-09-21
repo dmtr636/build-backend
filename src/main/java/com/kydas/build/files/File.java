31a51f79256d5659e657d9e16a91167e93becb34
dmtr636 <dmtr636@gmail.com>
2025-09-21 22:52:46 +0300

    fix file entity

--- src/main/java/com/kydas/build/files/File.java
+++ src/main/java/com/kydas/build/files/File.java
@@ -1,9 +1,6 @@
 package com.kydas.build.files;
 
-import com.kydas.build.core.crud.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,7 +13,11 @@ import java.util.UUID;
 @Setter
 @Entity
 @Table(name = "files")
-public class File extends BaseEntity {
+public class File {
+    @Id
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID id;
+
     @Column(nullable = false)
     private String originalFileName;
 
@@ -37,5 +38,30 @@ public class File extends BaseEntity {
 
     public enum Type {
         PROFILE_IMAGE, PROJECT_COVER_IMAGE, PROJECT_CONTENT_IMAGE, PROJECT_CONTENT_VIDEO, REPORT_ATTACHMENT_IMAGE, REVIEW_IMAGE
+    }
+
+    @Version
+    @Column(nullable = false)
+    private int version;
+
+    public String getEntityName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof File that)) return false;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "%s { id = %s }".formatted(getEntityName(), id);
     }
 }
